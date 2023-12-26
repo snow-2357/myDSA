3d58b3442341e3e2f32e730fa1670247f864c47c
simanta ray <simantaray89@gmail.com>
2023-12-26 10:20:15 +0530

    fix for maxsubarray with negative numbers

--- array/Medium/Max_subarray.js
+++ array/Medium/Max_subarray.js
@@ -1,18 +1,17 @@
 // Given an integer array nums, find the
 // subarray
 //  with the largest sum, and return its sum.
+// this has a  issue with all negative integer
 /**
  * @param {number[]} nums
  * @return {number}
  */
 var maxSubArray = function (nums) {
-  let max = 0;
+  let max = -Infinity;
   let temp = 0;
   for (let i = 0; i < nums.length; i++) {
     temp = temp + nums[i];
-    if (temp > max) {
-      max = temp;
-    }
+    max = Math.max(max, temp);
     if (temp < 0) {
       temp = 0;
     }
