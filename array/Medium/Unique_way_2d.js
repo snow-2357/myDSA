02eb2f9a672de9f4c6e6cfc5a8d45e0aaf9bfab4
simanta ray <simantaray89@gmail.com>
2023-12-28 17:57:41 +0530

    unique way

--- array/Medium/Unique_way_2d.js
+++ array/Medium/Unique_way_2d.js
@@ -1,13 +1,22 @@
-var uniquePaths = function (m, n) {
-  let x = 0,
-    y = 0;
-  if (x == m - 1 && y == n - 1) {
-    return 1;
-  }
-  if (x >= m || y >= n) return 0;
-  else {
-    return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
-  }
-}; // will fail in large values because of recursion
+// var uniquePaths = function (m, n) {
+//   let x = 0,
+//     y = 0;
+//   if (x == m - 1 && y == n - 1) {
+//     return 1;
+//   }
+//   if (x >= m || y >= n) return 0;
+//   else {
+//     return uniquePaths(m - 1, n) + uniquePaths(m, n - 1);
+//   }
+// }; // will fail in large values because of recursion,
+// we can modify it by keeping track where we already visited for the dp solution
 
-console.log(uniquePaths(23, 12));
+var uniquePaths = function (m, n) {
+  let maxNoOfStep = m + n - 2;
+  return (maxNoOfStep * (maxNoOfStep - 1)) / 2;
+};
+// we have to take m-1 + n-1 steps with right and down steps to find the
+// destination so its m+n-2 C 2  and from nCr fromula we can multiplay from n to n-r upon r to 1.
+
+console.log(uniquePaths(3, 7));
+// console.log(dp);
